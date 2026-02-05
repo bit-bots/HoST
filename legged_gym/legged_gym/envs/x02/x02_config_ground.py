89bc7d11cf56d9383f404562af573ae2c1b1998b
Miro Laptop <miroilija03@gmail.com>
2026-02-05 02:13:54 +0100

    lower_deviation erst ab phase 3, x02_training_dev

--- legged_gym/legged_gym/envs/x02/x02_config_ground.py
+++ legged_gym/legged_gym/envs/x02/x02_config_ground.py
@@ -284,7 +284,7 @@ class x02Cfg(LeggedRobotCfg):
             target_lin_vel_xy = 10
             target_feet_height_var = 2.5
             target_target_upper_dof_pos = 10
-            #target_target_lower_dof_pos = 10 #von uns hinzugefügt
+            target_target_lower_dof_pos = 15 #von uns hinzugefügt
             target_lower_body_deviation = 10 #soll symmetrie der Beine erzeugen
             target_target_orientation = 10
             target_target_base_height = 10

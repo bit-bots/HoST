2e2e3ba8c2b2391c2595c77b723429942d5ea1bf
Miro Laptop <miroilija03@gmail.com>
2026-04-22 12:07:21 +0200

    init pose changed, prone_training10 + supine_training8

--- legged_gym/legged_gym/envs/pi_plus/pi_plus_config_ground.py
+++ legged_gym/legged_gym/envs/pi_plus/pi_plus_config_ground.py
@@ -44,8 +44,8 @@ class Pi_PlusCfg( LeggedRobotCfg ):
             "l_ankle_pitch_joint": -0.0,
             "l_ankle_roll_joint": 0,
             #left arm (4 dof)
-            "l_shoulder_pitch_joint": 0.0,
-            "l_shoulder_roll_joint": 0.0,
+            "l_shoulder_pitch_joint": -1.57,
+            "l_shoulder_roll_joint": -1.57,
             "l_upper_arm_joint": 0.0,
             "l_elbow_joint": 0.0,
             # right leg (6 dof)
@@ -56,8 +56,8 @@ class Pi_PlusCfg( LeggedRobotCfg ):
             "r_ankle_pitch_joint": -0.0,
             "r_ankle_roll_joint": 0,
             #right arm (4 dof)
-            "r_shoulder_pitch_joint": 0.0,
-            "r_shoulder_roll_joint": 0.0,
+            "r_shoulder_pitch_joint": -1.57,
+            "r_shoulder_roll_joint": -1.57,
             "r_upper_arm_joint": 0.0,
             "r_elbow_joint": 0.0,
             #head (2 dof)
